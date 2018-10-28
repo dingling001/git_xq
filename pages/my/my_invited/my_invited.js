aa3a8b02aca28a8c6610b24e14a45a1b666e0982
dl_kaituo <215594238@qq.com>
2018-10-29 07:36:50 +0800

    小程序 10.26

--- pages/my/my_invited/my_invited.js
+++ pages/my/my_invited/my_invited.js
@@ -1,16 +1,12 @@
 // pages/my/my_invited/my_invited.js
+var network = require("../../../utils/network.js");
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
